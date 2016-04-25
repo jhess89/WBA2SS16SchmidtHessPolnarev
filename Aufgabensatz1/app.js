730e125ff00166fbcae893b795ad0a11bef62c17
marioschmidt94 <mario.schmidt@smail.fh-koeln.de>
2016-04-25 16:24:58 +0200

    Kommentare im Code hinzugefügt

--- Aufgabensatz1/app.js
+++ Aufgabensatz1/app.js
@@ -1,15 +1,25 @@
+// fs initialisieren
 var fs = require("fs");
 
+// App startet
 console.log("App gestartet \n");
 
+// Wolkenkratzer.json wird eingelesen
 fs.readFile("./wolkenkratzer.json", function (err, data) { 
     
+    // die Datei parsen 
     var jsonData = JSON.parse(data);
     
+    // Schleife um alle Elemente des JSON Elements in der app.js zu speichern
     for (var i = 0; i < jsonData.wolkenkratzer.length; i++) {
-    var counter = jsonData.wolkenkratzer[i];
-    console.log("Name: " + counter.name);
-    console.log("Stadt: " + counter.stadt);
-    console.log("Höhe: " + counter.hoehe + "m");
-    console.log("\n--------------------------------------\n"); 
-}});
+        
+        // Zuweisung der Elemente
+        var counter = jsonData.wolkenkratzer[i];
+    
+        // Ausgabesequenz
+        console.log("Name: " + counter.name);
+        console.log("Stadt: " + counter.stadt);
+        console.log("Höhe: " + counter.hoehe + "m");
+        console.log("\n--------------------------------------\n"); 
+    }
+});
