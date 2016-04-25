20da53519a7ad365b7d02106330adff8c2e76555
marioschmidt94 <mario.schmidt@smail.fh-koeln.de>
2016-04-25 15:54:33 +0200

    JSON-Datei enthält nur noch ein Element, Aufruf/Formatierung stimmt jetzt allerdings

--- Aufgabensatz1/app.js
+++ Aufgabensatz1/app.js
@@ -1,9 +1,17 @@
 var fs = require("fs");
 
-console.log("Starting");
+console.log("App gestartet \n");
 
-fs.readFile("./wolkenkratzer.json", function(error, data) {
-   console.log(data.toString());
+fs.readFile("./wolkenkratzer.json", function (err, data) { 
+    
+    var arr = JSON.parse(data);
+    
+    console.log("Name: " + arr.name);
+    console.log("Stadt: " + arr.stadt);
+    console.log("Höhe: " + arr.hoehe + "m");
+    console.log("--------------------\n");
+    
 });
+
 
 
